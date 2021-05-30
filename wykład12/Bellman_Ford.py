d45390fdb0ce7b311943cd095af382df6257caf7
Mateusz Powęska <mateuszpow7@gmail.com>
2021-05-30 15:59:24 +0200

    bellman ford

---
+++ wykład12/Bellman_Ford.py
@@ -0,0 +1,86 @@
+from math import inf
+
+#uwaga na ujemne cykle
+#graf musi byc skierowany
+#O(V^3) dla macierzowej
+#O(V*E) dla list sasiedztwa
+def bellman_ford(G,v): #reprezentacja macierzowa
+    n = len(G)
+    d = [inf]*n
+    parents = [None]*n
+    d[v] = 0
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if G[j][k] != 0 and d[k] > d[j] + G[j][k]:  # relaksacja
+                    d[k] = d[j] + G[j][k]
+                    parents[k] = j
+
+
+    for i in range(n):
+        for j in range(n):
+            if G[i][j] != 0 and d[j] > d[i] + G[i][j]: #weryfikacja czy jest ujemny cykl
+                return None
+
+    return d
+
+G = [[0,1,1,-2],
+     [1,0,3,2],
+     [1,3,0,1],
+     [-2,2,1,0]]
+# None
+'''G = [[0, -1, -1],
+    [-1, 0, 0],
+    [1, 0, 0]]'''
+
+# [0, 5, 5, 7, 9, 8]
+'''G = [[0, 0, 10, 0, 0, 8],
+         [0, 0, 0, 2, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 0, -2, 0, 0, 0],
+         [0, -4, 0, -1, 0, 0],
+         [0, 0, 0, 0, 1, 0]]'''
+
+#print(bellman_ford(G,0))
+
+def bellman_ford_lists(G,v): #reprezentacja listowa
+    n = len(G)
+    d = [inf]*n
+    parents = [None]*n
+    d[v] = 0
+
+    for i in range(n): #V
+        for u in range(n): #E
+            for v in G[u]:
+                if d[v[0]] > d[u] + v[1]:  # relaksacja
+                    d[v[0]] = d[u] + v[1]
+                    parents[v[0]] = u
+
+
+    for u in range(n):#E
+        for v in G[u]:
+            if d[v[0]] > d[u] + v[1]: #weryfikacja czy jest ujemny cykl
+                return None
+
+    return d
+
+# [0, 5, 5, 7, 9, 8]
+G = [[[5, 8], [2, 10]],
+    [[3, 2]],
+    [[1, 1]],
+    [[2, -2]],
+    [[1, -4], [3, -1]],
+    [[4, 1]]]
+# None
+'''G = [[[1, -1], [2, -1]],
+    [[0, -1]],
+    [[0, 1]]]'''
+
+# None
+'''G = [[[1, -4]],
+    [[2, -5]],
+    [[0, -2]],
+    [[4, 2]],
+    [[2, 1]]]'''
+print(bellman_ford_lists(G,0))
