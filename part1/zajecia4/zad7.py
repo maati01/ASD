f6fe5803cc1edd4b9e425c4dd0ac59e70e091668
Mateusz Powęska <mateuszpow7@gmail.com>
2021-08-07 16:46:25 +0200

    zad7

---
+++ part1/zajecia4/zad7.py
@@ -0,0 +1,64 @@
+from math import inf
+#na potrzeby testów kolory to cyfry od 0 do k - 1
+#algorytm przechodzi dwoma indeksami po tablicy wiec wiec O(n)
+
+def all_colors(A, k):
+    n = len(A)
+    i = 0
+    j = 0
+    result_i = 0
+    result_j = 0
+    the_best_result = inf
+    colors = [0] * k
+    counter = 0
+
+    while j < n:
+        while j < n:
+            colors[A[j]] += 1
+            if colors[A[j]] == 1:
+                counter += 1
+
+            if counter == k:
+                result = j - i
+                if result < the_best_result:
+                    the_best_result = result
+                    result_i = i
+                    result_j = j
+                    j += 1
+                break
+
+            else:
+                j += 1
+        while n > j >= i + k - 1:
+            if counter == k:
+                result = j - i
+                if result < the_best_result:
+                    the_best_result = result
+                    result_i = i
+                    result_j = j
+                else:
+                    colors[A[i]] -= 1
+                    if colors[A[i]] == 0:
+                        counter -= 1
+                        i += 1
+                        break
+                    else:
+                        i += 1
+
+            else:
+                colors[A[i]] -= 1
+                if colors[A[i]] == 0:
+                    counter -= 1
+                    break
+                i += 1
+
+    return the_best_result, result_i, result_j
+
+
+A = [1,2,0,0,3,3,3,0,2,1]
+B = [1,2,0,0,3,1,2,0,0,2]
+C = [0,1,2,3,4,5,1]
+
+print(all_colors(A,4))
+print(all_colors(B,4))
+print(all_colors(C,6))
