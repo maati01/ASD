07c4e344f3f53e05e4f687e6a85dc65a912bfef7
Mateusz Powęska <mateuszpow7@gmail.com>
2021-08-12 14:38:03 +0200

    zad5

--- part2/cwiczenia5/zad5.py
+++ part2/cwiczenia5/zad5.py
@@ -1,42 +1,33 @@
-#f(i,t) = max wartość podziału a1,..,ai na t ciągów
-#f(i,t) = min ( f(i - p, t - 1), suma od a(p + 1) do ai )
+# f(i,j) = max(min(f(i - p,j - 1),sum(i - p,i))), p (1, i + 1)
+# min value do itego indeksu na j podciągów
+#sprawdzam czy mogę stworzyć przedział który poprawi mi minimalny
 
-def better_sum(T,j,i): #można zrobić w czasie stałym odejmując sumy XD
-    sum_ = 0
-    for k in range(j,i):
-        sum_ += T[k]
+def maximin(A, k):
+    n = len(A)
+    sum_to_i = [0] * n
+    F = [[0] * (k + 1) for _ in range(n)]
 
-    return sum_
-
-def sequences(T,k):
-    n = len(T)
-    F = [[0]*(k + 1) for _ in range(n)]
-
-    val = 0
+    temp = 0
     for i in range(n):
-        val += T[i]
-        F[i][1] = val #jeden podciag jest suma wszystkich elementów
+        temp += A[i]
+        sum_to_i[i] += temp
+        F[i][1] = temp
 
+    for i in range(1,n):
+        for j in range(2, k + 1):
+            if i + 1 >= j:
+                for p in range(1, i + 1):
+                    temp_val = sum_to_i[i] - sum_to_i[i - p]
+                    temp_min = min(F[i - p][j - 1], temp_val)
+                    if temp_min > F[i][j]:
+                        F[i][j] = temp_min
 
-    min_val = 0
+    return F[n-1][k]
 
-    for i in range(n):
-        for j in range(2,k + 1):
-            for p in range(i):
-                val = min(F[i-p][j-1],better_sum(T,p + 1,i))
-                if val > min_val:
-                    min_val = val
-
-            F[i][j] = min_val
-            min_val = 0
-
-    for i in range(n):
-        for j in range(k+1):
-            print(F[i][j], end = " ")
-        print()
-
-
-k = 3
-#T = [5,6,4,3,12,1,6,7,8,7,7]
-T = [1,2,3,5,6]
-print(sequences(T,k))
+print(maximin([1, 2, 3, 5, 6], 3)) #5
+print(maximin([5, 6, 4, 3, 12, 1, 6, 7, 8, 7, 7], 3)) #18
+print(maximin([1, 2, 3, 5, 1, 6], 3)) #6
+print(maximin([1, 2, 3, 5, 1, 6], 4)) #3
+print(maximin([1, 2, 3, 5, 1, 6], 5)) #1
+print(maximin([1, 2, 3, 5, 1, 6], 6)) #1
+print(maximin([16, 3, 3, 3, 0, 1, 2, 3, 5, 1, 6,1000], 6)) #6
