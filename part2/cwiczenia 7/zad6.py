524846eb2b0f48f4f9835a66e539158d8313b5ed
Mateusz Powęska <mateuszpow7@gmail.com>
2021-08-19 15:19:26 +0200

    zad6

--- part2/cwiczenia 7/zad6.py
+++ part2/cwiczenia 7/zad6.py
@@ -2,8 +2,18 @@
 
 def sum_distance(A):
     n = len(A)
+    sum1 = 0
+    sum2 = 0
     if n % 2 != 0: return A[n//2]
-    else: return (A[n//2 - 1] + A[n//2])/2
+    else:
+        for i in range(n):
+            sum1 = abs(A[i] - A[n//2 - 1])
+            sum2 = abs(A[i] - A[n//2])
+
+        if sum1 > sum2:
+            return A[n//2]
+        else:
+            return A[n //2 - 1]
 
 A = [1,2,100]
 B = [1,2,50,100]
